06c8046b1001d727479c8d5438aea5ce9f837052
Maxim <titov3000@mail.ru>
2022-10-22 18:01:06 +0300

    Rest

--- js/script.js
+++ js/script.js
@@ -153,12 +153,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Используем классы для карточек
     class MenuItem {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -169,9 +170,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         render() {
-            const element = document.createElement('div')
+            const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML = `
-            <div class="menu__item">
                 <img src="${this.src}" alt="${this.alt}">
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -180,7 +186,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-            </div>
             `;
             this.parent.append(element);
         }
@@ -188,7 +193,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Можно такой синтаксис
     // const div = new MenuItem();
     // div.render();
-    
+
     // Можно такой
 
     new MenuItem(
@@ -197,7 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
     ).render();
 
     new MenuItem(
@@ -206,7 +211,9 @@ document.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         14,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
+        'big'
     ).render();
 
     new MenuItem(
@@ -215,9 +222,8 @@ document.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         21,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
+        'big'
     ).render();
-
-
-
 });
