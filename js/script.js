ae9d1a90a6022e812a7f5f9646e52fd0ee600327
Maxim <titov3000@mail.ru>
2022-10-27 13:35:11 +0300

    axios library

--- js/script.js
+++ js/script.js
@@ -203,34 +203,41 @@ document.addEventListener('DOMContentLoaded', () => {
     // Получаем массив объектов
     // getResource('http://localhost:3000/menu')
     //     .then(data => {
-    //         data.forEach(({ img, altimg, title, descr, price }) => {
-    //             new MenuItem(img, altimg, title, descr, price, '.menu .container').render();
-    //         });
+    // data.forEach(({ img, altimg, title, descr, price }) => {
+    //     new MenuItem(img, altimg, title, descr, price, '.menu .container').render();
+    // });
     //     });
 
-    getResource('http://localhost:3000/menu')
-        .then(data => createCard(data));
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
 
-    function createCard(data) {
-        data.forEach(({ img, altimg, title, descr, price }) => {
-            const element = document.createElement('div');
+    // function createCard(data) {
+    //     data.forEach(({ img, altimg, title, descr, price }) => {
+    //         const element = document.createElement('div');
 
-            element.classList.add('menu__item');
+    //         element.classList.add('menu__item');
 
-            element.innerHTML = `
-                <img src="${img}" alt="${altimg}">
-                    <h3 class="menu__item-subtitle">${title}</h3>
-                    <div class="menu__item-descr">${descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                    </div>
-                `;
+    //         element.innerHTML = `
+    //             <img src="${img}" alt="${altimg}">
+    //                 <h3 class="menu__item-subtitle">${title}</h3>
+    //                 <div class="menu__item-descr">${descr}</div>
+    //                 <div class="menu__item-divider"></div>
+    //                 <div class="menu__item-price">
+    //                     <div class="menu__item-cost">Цена:</div>
+    //                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //                 </div>
+    //             `;
 
-            document.querySelector('.menu .container').append(element);
+    //         document.querySelector('.menu .container').append(element);
+    //     });
+    // }
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({ img, altimg, title, descr, price }) => {
+                new MenuItem(img, altimg, title, descr, price, '.menu .container').render();
+            });g
         });
-    }
 
 
     // Forms
@@ -322,9 +329,4 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-    //  json-server db.json - генерирует локалхост
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
 });
